a401e946ca5770094095de43aab28e506c388b1b
DipalDonga <128701888+DipalDonga@users.noreply.github.com>
2023-06-02 14:54:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,55 +22,33 @@ include("connection.php");
     <?php
     $sql = "SELECT * FROM movieTable";
     ?>
-     <div class="services-container">
-        <?php
-$count=1;
-            $sql1 = mysqli_query($con,"select * from screen");
-            while($row = mysqli_fetch_array($sql1)){
-
-                $msql = mysqli_query($con,"select * from movie where mid =".$row['movie']);
-                $mrow = mysqli_fetch_array($msql);
-
-                echo '<div class="service-item">
-                <div class="service-item-icon">
-                    <i class="fas fa-4x fa-one"></i>
-                </div>
-               <a href="movie.php?htype='.$row['htype'].'"> <h2>Screen '.$count.'</h2></a>
-            </div>';
-$count++;
-            }
-
-        ?>
-        </div>
+     
     <div id="home-section-1" class="movie-show-container">
         <h1>Currently Showing</h1>
         <h3>Book a movie now</h3>
 
         <div class="movies-container">
 
-            <?php
-            if ($result = mysqli_query($con, $sql)) {
-                if (mysqli_num_rows($result) > 0) {
-                    for ($i = 0; $i <= 5; $i++) {
-                        $row = mysqli_fetch_array($result);
-                        echo '<div class="movie-box">';
-                        echo '<img src="' . $row['movieImg'] . '" alt=" ">';
-                        echo '<div class="movie-info ">';
-                        echo '<h3>' . $row['movieTitle'] . '</h3>';
-                        echo '<a href="booking.php?id=' . $row['movieID'] . '"><i class="fas fa-ticket-alt"></i> Book a seat</a>';
-                        echo '</div>';
-                        echo '</div>';
-                    }
-                    mysqli_free_result($result);
-                } else {
-                    echo '<h4 class="no-annot">No Booking to our movies right now</h4>';
-                }
-            } else {
-                echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
-            }
+              <?php
 
-            // Close connection
-            mysqli_close($con);
+            $sql1 = mysqli_query($con,"select * from movie");
+             while($mrow = mysqli_fetch_array($sql1)){
+
+
+                    
+
+                        echo '<div class="movie-box">
+                            <img src="img/' . $mrow['image'] . '" alt=" " height="500" width="500">
+                            <div class="movie-info ">
+                            <h3>' . $mrow['movieName'] . '</h3>
+                            <a href="booking.php?id=' . $mrow['mid'] . '"><i class="fas fa-ticket-alt"></i> Book a seat</a>
+                            </div>
+                            </div>';
+                   
+
+
+                }
+            
             ?>
         </div>
     </div>
@@ -102,54 +80,7 @@ $count++;
             <div class="service-item"></div>
         </div>
     </div>
-    <div id="home-section-3" class="trailers-section">
-        <h1 class="section-title">Explore new movies</h1>
-        <h3>Now showing</h3>
-        <div class="trailers-grid">
-            <div class="trailers-grid-item">
-                <img src="img/movie-thumb-1.jpg" alt="">
-                <div class="trailer-item-info" data-video="Z1BCujX3pw8">
-                    <h3>Captain Marvel</h3>
-                    <i class="far fa-3x fa-play-circle"></i>
-                </div>
-            </div>
-            <div class="trailers-grid-item">
-                <img src="img/movie-thumb-2.jpg" alt="">
-                <div class="trailer-item-info" data-video="OPEfsEaFv_c">
-                    <h3>Karmat Bytmrmt</h3>
-                    <i class="far fa-3x fa-play-circle"></i>
-                </div>
-            </div>
-            <div class="trailers-grid-item">
-                <img src="img/movie-thumb-3.jpg" alt="">
-                <div class="trailer-item-info" data-video="cksYkEzUa7k">
-                    <h3>The Lego Movie</h3>
-                    <i class="far fa-3x fa-play-circle"></i>
-                </div>
-            </div>
-            <div class="trailers-grid-item">
-                <img src="img/movie-thumb-4.jpg" alt="">
-                <div class="trailer-item-info" data-video="Ze5YA4mkzhI">
-                    <h3>Secret Men Club</h3>
-                    <i class="far fa-3x fa-play-circle"></i>
-                </div>
-            </div>
-            <div class="trailers-grid-item">
-                <img src="img/movie-thumb-5.jpg" alt="">
-                <div class="trailer-item-info" data-video="lbUAVTc7NTo">
-                    <h3>VICE</h3>
-                    <i class="far fa-3x fa-play-circle"></i>
-                </div>
-            </div>
-            <div class="trailers-grid-item">
-                <img src="img/movie-thumb-6.jpg" alt="">
-                <div class="trailer-item-info" data-video="RyFlfN4dD14">
-                    <h3>The Vanishing</h3>
-                    <i class="far fa-3x fa-play-circle"></i>
-                </div>
-            </div>
-        </div>
-    </div>
+   
     <footer></footer>
 
     <script src="scripts/jquery-3.3.1.min.js "></script>
