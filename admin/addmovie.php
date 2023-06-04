d2475339c7455909dc86e1d1986e1d2be588947b
DipalDonga <128701888+DipalDonga@users.noreply.github.com>
2023-06-04 22:11:08 +0530

    Update addmovie.php

--- admin/addmovie.php
+++ admin/addmovie.php
@@ -27,7 +27,7 @@ if (isset($_POST['but_logout'])) {
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-
+ 
 <body>
     <?php
     $sql = "SELECT * FROM bookingTable";
