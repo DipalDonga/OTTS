873619ffe3d9446731e8f2553ba1049fbae9c20d
DipalDonga <128701888+DipalDonga@users.noreply.github.com>
2023-06-10 03:10:48 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,30 +1,27 @@
   
-  <a href="index.php">
-        <h1 class="navbar-heading">ATSE Cinema</h1>
-    </a>  
+<a href="index.php">
+    <h1 class="navbar-heading">ATSE Cinema</h1>
+</a>
 <div class="navbar-">
     <nav class="navbar">
         <ul class="navbar-menu">
             <li><a href="index.php">Home</a></li>
-            <li><a href="schedule.php">Schedule</a></li>
             <!-- <li><a href="TxnStatus.php" target="_blank">Status</a></li> -->
-            <li><a href="#">Team</a></li>           
             <li><a href="contact-us.php">Contact</a></li>
-            <li><?php 
-            global $web;
-                            if(isset($_SESSION['moviemin_id'])){
-
-                                $login = '<a href="#">'. $_SESSION['moviemin_name'].'</a>
-
-                                    
-                                    <li><a href="includes/allfunction.php?action=signout"><i class="fa fa-sign-out">Logout</i></a></li>';
-                            }else{
-                                $login = '<a href='.$web.'login.php>Login</a>';
-                            }
-                            echo $login;
-
-                            
-                        ?></li>
+            <li><a href="booking-list.php">Booking</a></li>
+            <li>
+            <?php
+                global $web;
+                if(isset($_SESSION['moviemin_id'])){
+                    $login = '<a href="#">'. $_SESSION['moviemin_name'].'</a>
+                        <li><a href="booking-list.php">Booking</a></li>
+                        <li><a href="includes/allfunction.php?action=signout"><i class="fa fa-sign-out">Logout</i></a></li>';
+                }else{
+                    $login = '<a href='.$web.'login.php>Login</a>';
+                }
+                echo $login;
+            ?>
+            </li>
         </ul>
     </nav>
 </div>
