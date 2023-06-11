ad730dd78a8008981f99c19087a0c0226e36eafb
DipalDonga <128701888+DipalDonga@users.noreply.github.com>
2023-06-11 23:56:18 +0530

    Add files via upload

---
+++ payment_done.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+include("connection.php");
+?>
+
+<html lang="en">
+<body>
+
+<p style="text-align: center;"><img src="https://cdn.jotfor.ms/img/check-icon.png" alt="creating custom PHP thank you page Image-0" width="128" height="128" /></p>
+<div style="text-align: center;">
+<h1 style="text-align: center;">Thank You!</h1>
+<p>
+
+
+</p>
+<p style="text-align: center;">Your seat has been booked.</p>
+
+ <button type="submit" id="submitButton" value="save" name="submit" class="form-btn"><a href="index.php">Back to Home</a></button>
+</div>
+</body>
+</html>
